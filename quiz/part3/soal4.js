caf30fc276b62db9d7cb8bff9944807dde0a97fa
Haidir Aditya <haidiraditya16@gmail.com>
2025-09-19 16:59:04 +0800

    Alhamdulillah, akhirnya berhasil mengerjakan seluruh quiz di semua part, dan juga ujian-nya yang sangat mengocok pikiran ☠️☠️☠️

--- quiz/part3/soal4.js
+++ quiz/part3/soal4.js
@@ -1,5 +1,21 @@
 function tentukanDeretGeometri(arr) {
   // you can only write your code here!
+  
+  // tidak ada angka yang bisa di deret karena isi array tidak lebih dari 2
+  if (arr.length < 2) return false;
+  
+  // tentukan rasio
+  let rasio = Math.abs(arr[1] / arr[0]);
+
+  for (let i = 0; i < arr.length; i++) {
+    // skip index pertama karena tidak index dibelakangnya yang bisa dibagi
+    if (!arr[i - 1]) continue;
+    
+    // jika index didepan dibagi index sekarang sama dengan rasio maka false
+    if ((arr[i] / arr[i - 1]) !== rasio) return false;
+  }
+
+  return true;
 }
 
 // TEST CASES
